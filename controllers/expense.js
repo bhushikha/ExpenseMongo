f24569a6a909d880b3a27fad4713b8f8d5d350bd
bhushikha <shikhamishrabhu21@gmail.com>
2023-11-07 00:23:16 +0530

    Mongoose

---
+++ controllers/expense.js
@@ -0,0 +1,48 @@
+const Expense = require('../models/expenses');
+function isstringinvalid(string) {
+    if (string == undefined || string.length == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+exports.postExpense = async (req, res, next) => {
+    try {
+        const { expenseamount, description, category } = req.body;
+        if (isstringinvalid(expenseamount) || isstringinvalid(description) || isstringinvalid(category)) {
+            return res.status(400).json({ success: false, message: 'parameter is missing' })
+        }
+        const expense = new Expense({ expenseamount, description, category, userId: req.user })
+        expense.save();
+        res.status(201).json({ expense, success: true })
+    }
+    catch {
+        err => {
+            return res.status(500).json({ success: false, error: err })
+        }
+    }
+
+}
+exports.getExpense = async (req, res, next) => {
+    try {
+        const expenses = await Expense.find({ userId: req.user._id })
+
+        res.status(200).json({ expenses, success: true })
+    } catch (err) {
+        res.status(500).json({ error: err, success: false })
+    }
+}
+exports.deleteExpense = async (req, res, next) => {
+    try {
+        const expenseid = req.params.expenseid;
+        console.log(expenseid)
+        if (isstringinvalid(expenseid)) {
+            return res.status(400).json({ success: false, message: 'bad parameter' })
+        }
+        const expense = await Expense.deleteOne({ _id: expenseid, userId: req.user })
+        res.status(200).json({ success: true, message: 'Deleted Successfully' })
+    }
+    catch (err) {
+        res.status(500).json({ message: 'internal Error', success: false })
+    }
+}
---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+
+
+const auth = async (req, res, next) => {
+    try {
+
+        const token = req.header('Authorization');
+        console.log(token)
+        const user = jwt.verify(token, process.env.TOKEN)
+        console.log('auth', user.userId)
+        User.findById(user.userId).then((user => {
+            console.log('userrrr', user._id)
+            req.user = user;
+            console.log(req.user._id)
+            console.log(req.user)
+            next();
+        }))
+
+            .catch(err => {
+                return res.status(500).json({ success: false })
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ success: false })
+
+    }
+
+}
+module.exports = auth;
